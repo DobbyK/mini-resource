fd3303f189990b59b84961ad006aaf55479ac7a6
DobbyK <69256884+DobbyK@users.noreply.github.com>
2024-09-30 16:25:13 -0700

    added per second

--- index/script.js
+++ index/script.js
@@ -3,6 +3,7 @@ var buildings = { farm: 0, lumbermill: 0, quarry: 0 };
 var prices = { farm: 10, lumbermill: 10, quarry: 10 };
 var level = { farm: 1, lumbermill: 1, quarry: 1 };
 var levelprices = { farm: 20, lumbermill: 20, quarry: 20 };
+var gains = { food: 0, wood: 0, stone: 0 }
 
 function collect(resource) {
     resources[resource] += 1;
@@ -14,6 +15,7 @@ function build(building, resource) {
         resources[resource] -= prices[building];
         buildings[building] += 1;
         prices[building] = Math.floor(1.25 * prices[building]);
+        gains[resource] = buildings[building] * level[building];
         updateScreen();
     } else {
         alert(`Not enough ${resource} to build a ${building}.`);
@@ -25,6 +27,7 @@ function levelUp(building, resource) {
         resources[resource] -= levelprices[building];
         level[building] += 1;
         levelprices[building] = Math.floor(1.5 * levelprices[building]);
+        gains[resource] = buildings[building] * level[building];
         updateScreen();
     } else {
         alert(`Not enough ${resource} to upgrade ${building}.`);
@@ -32,20 +35,22 @@ function levelUp(building, resource) {
 }
 
 function updateScreen() {
-    for (let key in resources) {
-        document.getElementById(key).innerHTML = resources[key];
+    for (let i in resources) {
+        document.getElementById(i).innerHTML = resources[i];
+        document.getElementById(i + "ps").innerHTML = gains[i];
+
     }
-    for (let key in buildings) {
-        document.getElementById(key).innerHTML = buildings[key];
-        document.getElementById(key + "price").innerHTML = prices[key];
-        document.getElementById(key + "_lvl").innerHTML = level[key];
-        document.getElementById(key + "Up").innerHTML = levelprices[key];
+    for (let i in buildings) {
+        document.getElementById(i).innerHTML = buildings[i];
+        document.getElementById(i + "price").innerHTML = prices[i];
+        document.getElementById(i + "_lvl").innerHTML = level[i];
+        document.getElementById(i + "Up").innerHTML = levelprices[i];
     }
 }
 
 setInterval(() => {
-    resources.food += buildings.farm * level.farm;
-    resources.wood += buildings.lumbermill * level.lumbermill;
-    resources.stone += buildings.quarry * level.quarry;
+    for (let i in resources) {
+        resources[i] += gains[i];
+    }
     updateScreen();
 }, 1000);
