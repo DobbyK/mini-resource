f542147b2eed2dd86d84a87bb235cea5516edd7d
DobbyK <dobbyk@techie.com>
2025-05-12 16:57:01 -0700

    production tooltips, i might add them better to show all calculations

--- index/script.js
+++ index/script.js
@@ -518,7 +518,7 @@ function updateUI() {
                     <div class="tooltip">
                     <span class="tooltiptext">${res.tooltip}</span>
                     <strong>${name}</strong>: </div> <span id="${name}_amount">${res.amount}/${res.max}</span> 
-                    (+<span id="${name}_gain">${res.gain}</span>/s)
+                    <div class="tooltip"><span class="tooltiptext">+${res.gain+res}, -${res.loss}</span>(+<span id="${name}_gain">${res.gain}</span>/s)</div>
                     ${res.collectible ? `<button onclick="collect('${name}')">Collect</button>` : ''}
 ${res.sellable ? `<button onclick="sell('${name}')">Sell $${res.worth}</button>` : ''}
 
