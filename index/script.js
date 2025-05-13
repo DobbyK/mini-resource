269e6384c3e7cf6ecfd8968c58951add35acbc8f
DobbyK <dobbyk@techie.com>
2025-05-12 17:15:31 -0700

    break in line for buildings

--- index/script.js
+++ index/script.js
@@ -557,7 +557,7 @@ ${res.sellable ? `<button onclick="sell('${name}')">Sell $${res.worth}</button>`
                         <span class="tooltiptext">${building.tooltip}</span>
                         <strong>${building.name}</strong>
                     </div> (Lv ${building.level}) - Count: <span id="${building.name}_count">${building.count}</span>
-                    <button onclick="build('${building.name}')">Build (${formatCost(building.buildCost)}${costText})</button>                   
+                    <br><button onclick="build('${building.name}')">Build (${formatCost(building.buildCost)}${costText})</button>                   
                 </div>
             `;
         }
