32c6c34ba377983d2aa3733b761ef790d75c0acb
DobbyK <dobbyk@techie.com>
2025-05-14 17:32:03 -0700

    changelog

--- index/script.js
+++ index/script.js
@@ -721,7 +721,7 @@ function initGame() {
             <input type="file" id="importFile" accept=".json" style="display:none" onchange="importSave(event)">
             <button onclick="document.getElementById('importFile').click()">Import Save</button>
             <button onclick="giveAllResourcesDebug()">Don't Press</button>
-            <a target="_blank" href="changelog.html">v0.0.7</a>
+            <a target="_blank" href="changelog.html">v0.0.8</a>
         </div>
         <div id="resources">
         </div>
