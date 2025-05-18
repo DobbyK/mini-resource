105e79bbbbfc96694e1c31c6bdb38fd9ae127b63
DobbyK <dobbyk@techie.com>
2025-05-17 21:35:04 -0700

    added tomatoes and iron, use for territory.

--- index/script.js
+++ index/script.js
@@ -57,6 +57,11 @@ const gameData = {
             unlocked: false, collectible: false, sellable: true,
             tooltip: "Not safe for babies? Too bad"
         },
+        tomato: {
+            max: 100, amount: 0, gain: 0, loss: 0, worth: 40,
+            unlocked: false, collectible: false, sellable: true,
+            tooltip: "Fruit or Veggie"
+        },
         meal: {
             max: 100, amount: 0, gain: 0, loss: 0, worth: 150,
             unlocked: false, collectible: false, sellable: true,
@@ -76,6 +81,11 @@ const gameData = {
             max: 100, amount: 0, gain: 0, loss: 0, worth: 10,
             unlocked: false, collectible: true, sellable: true,
             tooltip: "Non-descript resource of stuff mined underground. Also buildin."
+        },
+        iron: {
+            max: 100, amount: 0, gain: 0, loss: 0, worth: 70,
+            unlocked: false, collectible: true, sellable: true,
+            tooltip: "Fe(iend), specific metal"
         },
         leather: {
             max: 100, amount: 0, gain: 0, loss: 0, worth: 100,
@@ -109,32 +119,48 @@ const gameData = {
         }
     },
     buildings: {
+        ironMine: {
+            type: "iron", count: 0, gain: 0, unlocked: false, maxBoost: 50, production: 1,
+            buildCost: { territory: 100, stone: 200, metal: 500 },
+            resourcePrice: { },
+            buildingBoost: {},
+            scale: { territory: 1.05, stone: 1.05, metal: 1.05 },
+            tooltip: "You need a new mine for god knows what reason "
+        },
+        garden: {
+            type: "tomato", count: 0, gain: 0, unlocked: false, maxBoost: 50, production: 1,
+            buildCost: { territory: 50, wheat: 500, leather: 50 },
+            resourcePrice: { },
+            buildingBoost: {},
+            scale: { territory: 1.05, wheat: 1.05, leather: 1.05 },
+            tooltip: "Grown Organically"
+        },
         leatherSmith: {
-            type: "armor", count: 0, gain: 0, unlocked: false, maxBoost: 0, production: 1,
+            type: "armor", count: 0, gain: 0, unlocked: false, maxBoost: 50, production: 1,
             buildCost: { wood: 800, stone: 600, leather: 20 },
             resourcePrice: { leather: 2, human: 1 },
             buildingBoost: {},
             scale: { wood: 1.05, stone: 1.05, leather: 1.05 },
-            tooltip: "Turns hides into pride"
+            tooltip: "Turns hide into pride"
         },
         barracks: {
-            type: "soldier", count: 0, gain: 0, unlocked: false, maxBoost: 0, production: 0.5,
+            type: "soldier", count: 0, gain: 0, unlocked: false, maxBoost: 50, production: 0.5,
             buildCost: { wood: 3000, metal: 600, milk: 5 },
             resourcePrice: { armor: 1, family: 1, meal: 2 },
             buildingBoost: {},
-            scale: { wood: 1.05, metal: 1.05 },
-            tooltip: "The cradle of your army"
+            scale: { wood: 1.05, metal: 1.05, milk: 1.05 },
+            tooltip: "Make war not love"
         },
         pentagon: {
-            type: "territory", count: 0, gain: 0, unlocked: false, maxBoost: 0, production: 0.2,
+            type: "territory", count: 0, gain: 0, unlocked: false, maxBoost: 100, production: 0.2,
             buildCost: { metal: 800, stone: 1000 },
             resourcePrice: { soldier: 2 },
             buildingBoost: {},
             scale: { metal: 1.05, stone: 1.05 },
-            tooltip: "Expand and control what you claim"
+            tooltip: "jessie wake up"
         },
         pen: {
-            type: "cow", count: 0, gain: 1, unlocked: false, maxBoost: 5, production: 1, gain: 0,
+            type: "cow", count: 0, gain: 1, unlocked: false, maxBoost: 20, production: 1, gain: 0,
             buildCost: { wood: 2000, stone: 1500, wheat: 900 },
             resourcePrice: { human: 1, wheat: 5 },
             buildingBoost: {},
@@ -408,6 +434,30 @@ const gameData = {
     },
 
     research: {
+        unlockIronMine: {
+            name: "Metal Squared",
+            description: "No clothing here to iron, or steel. (thanks dad)",
+            cost: { science: 1000, stone: 2000, wheat: 1750, rat: 100, money: 5000, territory: 10, family: 50 },
+            effect: () => {
+                gameData.buildings.ironMine.unlocked = true;
+                gameData.resources.iron.unlocked = true;
+            },
+            completed: false,
+            requires: ["unlockGarden"],
+            tooltip: "Unlock Iron & Iron Mines"
+        },
+        unlockGarden: {
+            name: "New Croping tool",
+            description: "Photoshop v1.1: Tomato Revoultion",
+            cost: { science: 800, stone: 1500, wheat: 1750, milk: 100, money: 5000, territory: 10, family: 50 },
+            effect: () => {
+                gameData.buildings.garden.unlocked = true;
+                gameData.resources.tomato.unlocked = true;
+            },
+            completed: false,
+            requires: ["unlockWar"],
+            tooltip: "Unlock Tomatoes & Gardens"
+        },
         unlockArmor: {
             name: "Wear Cows",
             description: "I ain't wearing my food, you are!",
